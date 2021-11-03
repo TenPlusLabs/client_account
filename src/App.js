5cc27bbd378b6484d85403365e17ce3f68656d92
Lizzy223 <koladeelizabeth01@gmail.com>
2021-11-03 02:50:41 +0100

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
 
-function App() {
+
+const App = () => {
+
+const [sidebarOpen, setSidebarOpen] = useState(false);
+
+const openSidebar = () =>{
+  setSidebarOpen(true);
+}
+const closeSidebar = () =>{
+  setSidebarOpen(false);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>TenPlus</h1>
+      <Sidebar sidebarOpen={sidebarOpen} openSidebar={openSidebar}/>
+      <div className='main-content'>
+        <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar}/>
+      </div>
     </div>
   );
 }
