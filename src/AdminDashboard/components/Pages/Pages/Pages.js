5a069d80f87dbabd876e4fd8749dce43e9a36407
Makanjuola Martins <martins423love@gmail.com>
2021-12-11 17:29:44 +0100

    Added the Add new button and modal

--- src/AdminDashboard/components/Pages/Pages/Pages.js
+++ src/AdminDashboard/components/Pages/Pages/Pages.js
@@ -3,9 +3,14 @@ import './Pages.css'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { DataGrid } from '@mui/x-data-grid';
+// import Button from '@mui/material/Button';
+// import DeleteIcon from '@mui/icons-material/Delete';
+// import SendIcon from '@mui/icons-material/Send';
+// import Stack from '@mui/material/Stack';
+import BasicModal from './Modal';
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'id', headerName: 'ID', width: 50 },
     { field: 'pageTitle', headerName: 'Page Title', width: 130 },
     { field: 'pageURL', headerName: 'Page URL', width: 130 },
     { field: 'datePublished', headerName: 'Date Published', width: 90, },
@@ -17,9 +22,9 @@ const columns = [
         }`,
     },
     { field: 'metaData', headerName: 'Meta data', width: 90, },
-    { field: 'todayVisit', headerName: 'Number of visit"s today', width: 90, },
-    { field: 'oneWeekVisit', headerName: 'Number of visit"s in 7 days', width: 90, },
-    { field: 'oneMonthVisit', headerName: 'Number of visit"s in 1 month', width: 90, },
+    { field: 'todayVisit', headerName: 'No of visit"s today', width: 90, },
+    { field: 'oneWeekVisit', headerName: 'No of visit"s in 7 days', width: 90, },
+    { field: 'oneMonthVisit', headerName: 'No of visit"s in 1 month', width: 90, },
   ];
   
   const rows = [
@@ -43,21 +48,29 @@ function Pages() {
                         <div className='float-left d-flex justify-content-between'>
                             <div className=' '>
                                 {/* <img src='' alt='' width='20rem' height='2rem' className='img-fluid' /> */}
-                                <div style={{width:'50px', height:'50px', background:'blue', borderRadius:'50%', content:'a'}} />
+                                <div className='mt-3' style={{width:'50px', height:'50px', background:'blue', borderRadius:'50%', content:'a'}} />
 
                             </div>
-                            <div className='ml-3'>
+                            <div className='ml-3 mt-4'>
                                 <h2>Pages</h2>
                                 {/* <p className='text-grey'>17th ,January 2021</p> */}
                             </div>
                         </div>
                         <div className='float-right d-flex'>
-                            <div className='border rounded m-2 pt-n3'>
+                            {/* <div className='border rounded m-2 pt-n3'>
                                 <i className='las la-calender'/>
                                 <p>07/01/2021 - 04/01/2021</p>
                             </div>
                             <div className='border rounded d-flex align-items-center m-2'>
                                 <span className='las la-bars'></span>
+                            </div> */}
+                            <div>
+                                <BasicModal />
+                                {/* <Button className="mt-4"  variant="outlined"  startIcon={<AddCircleTwoToneIcon />}>
+                                    Add New
+                                    
+                                </Button> */}
+                                
                             </div>
                         </div>                        
                     </div>
@@ -68,17 +81,28 @@ function Pages() {
                                 <Tab>Published </Tab>
                                 <Tab>Draft</Tab>
                                 <Tab>Bin</Tab>
+                                <Tab>
+                                    
+                                </Tab>
                             </TabList>
 
                             <TabPanel>
-                                <div style={{ height: 400, width: '100%' }}>
+                                <div style={{ height: 450, width: '100%' }}>
                                     <DataGrid
                                         rows={rows}
                                         columns={columns}
-                                        pageSize={5}
+                                        pageSize={6}
                                         rowsPerPageOptions={[5]}
-                                        checkboxSelection
+                                        // checkboxSelection
                                     />
+                                    {/* <Stack direction="row" spacing={2}>
+                                        <Button variant="outlined" startIcon={<DeleteIcon />}>
+                                            Delete
+                                        </Button>
+                                        <Button variant="contained" endIcon={<SendIcon />}>
+                                            Send
+                                        </Button>
+                                    </Stack> */}
                                 </div>
                             </TabPanel>
 
