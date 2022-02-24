e195a957eb7933fc2837bd407f2b16b11e29abaf
Lizzy223 <koladeelizabeth01@gmail.com>
2022-02-24 12:50:35 +0100

    payment

--- src/components/Pages/SavedItem/Cart.js
+++ src/components/Pages/SavedItem/Cart.js
@@ -7,7 +7,7 @@ const Cart = () => {
     <div class='container-fluid cards p-5'>
         <h3 class='mb-3 mt-2 p-2'>Cart(3)</h3>
         <hr/>
-        <div class='row justify-content-between'>
+        <div class='row justify-content-between mt-3'>
             <div class='col-md-3 col-sm-6 shadow p-4'>
                 <div class='row'>
                     <div class='col-md-12'>
